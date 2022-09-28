49f721de6b7e95c38c852ffb1af313daa4e156f3
Jere Lorente <jlorentep@iesch.org>
2022-09-28 20:55:09 +0200

    Añadido idioma ingles

--- app/src/main/java/org/iesch/mi_calculadora/MainActivity.java
+++ app/src/main/java/org/iesch/mi_calculadora/MainActivity.java
@@ -25,45 +25,77 @@ public class MainActivity extends AppCompatActivity {
         EditText editNum2 = binding.num2Edit;
         Button btnSum = binding.sumarBtn;
         Button btnRest = binding.restarBtn;
-        Button btnMult= binding.multiplicarBtn;
+        Button btnMult = binding.multiplicarBtn;
         Button btnDiv = binding.dividirBtn;
         TextView resultadoText = binding.resultadoEdit;
-       // TextView resultadoText = findViewById(R.id.resultadoEdit);
+
 
         btnSum.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                /*
-                String num1=editNum1.getText().toString();
-                String num2=editNum2.getText().toString();
 
-                  if (!num1.isEmpty()||!num2.isEmpty()) {
-                    int num1Integer = Integer.parseInt(num1);
-                    int num2Integer = Integer.parseInt(num2);
+                if (!editNum1.getText().toString().isEmpty() && !editNum2.getText().toString().isEmpty()) {
 
-                    int resultado = num1Integer + num2Integer;
-
-                      //String resultadoTexto = getString(resultado);
-                      //resultadoText.setText(getString(resultado));
-                      binding.resultadoEdit.setText(resultado);
-                  }else {
-
-                      Toast.makeText(MainActivity.this, "Error", Toast.LENGTH_SHORT).show();
-                  }
-                */
-                if(!editNum1.getText().toString().isEmpty() && !editNum2.getText().toString().isEmpty()){
-
-                int num1 = Integer.parseInt(editNum1.getText().toString());
-                int num2 = Integer.parseInt(editNum2.getText().toString());
+                    int num1 = Integer.parseInt(editNum1.getText().toString());
+                    int num2 = Integer.parseInt(editNum2.getText().toString());
 
                     int resultado = num1 + num2;
                     resultadoText.setText(Integer.toString(resultado));
 
-                }else{
+                } else {
                     Toast.makeText(MainActivity.this, "No puedes dejar un campo vacio", Toast.LENGTH_SHORT).show();
                 }
+            }
+        });
+        btnRest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
+                if (!editNum1.getText().toString().isEmpty() && !editNum2.getText().toString().isEmpty()) {
 
+                    int num1 = Integer.parseInt(editNum1.getText().toString());
+                    int num2 = Integer.parseInt(editNum2.getText().toString());
+
+                    int resultado = num1 - num2;
+                    resultadoText.setText(Integer.toString(resultado));
+
+                } else {
+                    Toast.makeText(MainActivity.this, "No puedes dejar un campo vacio", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        btnMult.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if (!editNum1.getText().toString().isEmpty() && !editNum2.getText().toString().isEmpty()) {
+
+                    int num1 = Integer.parseInt(editNum1.getText().toString());
+                    int num2 = Integer.parseInt(editNum2.getText().toString());
+
+                    int resultado = num1 * num2;
+                    resultadoText.setText(Integer.toString(resultado));
+
+                } else {
+                    Toast.makeText(MainActivity.this, "No puedes dejar un campo vacio", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        btnDiv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if (!editNum1.getText().toString().isEmpty() && !editNum2.getText().toString().isEmpty()) {
+
+                    double num1 = Double.parseDouble(editNum1.getText().toString());
+                    double num2 = Double.parseDouble(editNum2.getText().toString());
+
+                    double resultado = num1 / num2;
+                    resultadoText.setText(Double.toString(resultado));
+
+                } else {
+                    Toast.makeText(MainActivity.this, "No puedes dejar un campo vacio", Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
